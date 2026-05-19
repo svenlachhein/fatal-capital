e50806c8bc6bddc40606faad552739192d3d2b33
svenlachhein <lachhein@drlc.de>
2026-05-19 20:59:34 +0200

    feat(pull-quote): FC Zitat

---
+++ components/PullQuote.tsx
@@ -0,0 +1,11 @@
+export default function PullQuote() {
+  return (
+    <section className="section section-alt">
+      <div className="section-inner">
+        <blockquote className="pull-quote">
+          Entscheidest du über Kapital oder entscheidet das Kapital über dich?
+        </blockquote>
+      </div>
+    </section>
+  );
+}
